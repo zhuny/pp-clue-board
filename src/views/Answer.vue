2ab5284beffbc2f7f8a5c5a9745464905c234db6
zhuny <zhuny@kaist.ac.kr>
2020-09-06 23:39:54 +0900

    add button

--- src/views/Answer.vue
+++ src/views/Answer.vue
@@ -5,18 +5,38 @@
         <tr>
           <th>순번</th>
           <th>토큰</th>
-          <th>장소</th>
           <th>흉기</th>
+          <th>장소</th>
           <th>질문자</th>
           <th>답변자</th>
         </tr>
       </thead>
+      <tbody>
+        <tr
+          v-for="(answer, index) in game_input.answer_list"
+          v-bind:key="index"
+        >
+          <td>{{ index }}</td>
+        </tr>
+      </tbody>
     </table>
+    <button v-on:click="add_answer()">추가</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Answer"
+  name: "Answer",
+  methods: {
+    add_answer() {
+      this.game_input.answer_list.push({
+        token: "",
+        weapon: "",
+        room: "",
+        questioner: "",
+        answerer: ""
+      });
+    }
+  }
 };
 </script>
